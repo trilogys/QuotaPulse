34ef16e5009b34b7be9391ae149105abd7a810f7
trilogys <135145953+trilogys@users.noreply.github.com>
2026-08-27 18:46:17 +0800

    Request iOS quota notification permission

--- AIQuotaApp/AIQuotaApp.swift
+++ AIQuotaApp/AIQuotaApp.swift
@@ -5,8 +5,11 @@ struct AIQuotaApp: App {
   var body: some Scene {
     WindowGroup {
       ContentView()
+        .task {
+          await QuotaNotifier.shared.requestAuthorization()
+        }
         .onOpenURL { url in
-          // Reserved for future deep links such as adquota://account/<uuid>.
+          // Reserved for future deep links such as aiquota://account/<uuid>.
           _ = url
         }
     }
