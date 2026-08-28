4315373dd7fddc960d4aa7a581113e05355d3053
trilogys <135145953+trilogys@users.noreply.github.com>
2026-08-28 15:09:45 +0800

    Fix iOS 16 widget bundle

--- AIQuotaWidget/AIQuotaWidgetBundle.swift
+++ AIQuotaWidget/AIQuotaWidgetBundle.swift
@@ -4,10 +4,6 @@ import WidgetKit
 @main
 struct AIQuotaWidgetBundle: WidgetBundle {
   var body: some Widget {
-    if #available(iOS 17.0, *) {
-      AIQuotaInteractiveWidget()
-    } else {
-      AIQuotaWidget()
-    }
+    AIQuotaWidget()
   }
 }
